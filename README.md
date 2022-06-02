cba4df5d0177471fdf9da64921fb00203787069f
Marcelo Freitas <marcelo.batera@gmail.com>
2022-06-02 10:49:21 +0900

    lsp: adds Tex and Markdown support

---
+++ README.md
@@ -0,0 +1,8 @@
+# Neovim config
+
+
+This is my own personal neovim config, used with neovim 0.8.0 in mind.
+
+I am doing this from scratch instead of doing some config builder or some out
+of the box IDE that uses he same things in order to understand how things work
+and do proper customization of this.
--- lua/marcelocf/lsp.lua
+++ lua/marcelocf/lsp.lua
@@ -33,7 +33,13 @@ end
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'pyright', 'rust_analyzer', 'tsserver', 'sumneko_lua' }
+local servers = { 
+  'ltex',           -- *TeX and Markdown
+  'pyright',        -- python
+  'rust_analyzer',  -- rust
+  'sumneko_lua',    -- lua
+  'tsserver',       -- typescrypt
+}
 for _, lsp in pairs(servers) do
   require('lspconfig')[lsp].setup {
     capabilities = capabilities,
