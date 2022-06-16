a78ef5f49c0d7c7caa450d7ad8ef16c714d46da9
Marcelo Freitas <marcelo.freitas@woven-planet.global>
2022-06-16 18:33:48 +0900

    i3: doesn't fail if iface is not connected

--- i3/load-daemons.sh
+++ i3/load-daemons.sh
@@ -6,7 +6,7 @@ setxkbmap -layout jp &
 ibus-daemon -d &
 picom -CGb --experimental-backends &
 nitrogen --restore &
-pactl set-default-sink alsa_output.usb-Yamaha_Corporation_AG06_AG03-00.analog-stereo
+pactl set-default-sink alsa_output.usb-Yamaha_Corporation_AG06_AG03-00.analog-stereo || echo ignoring
 
 ( sleep 5 && ibus engine mozc-jp ) &
 
