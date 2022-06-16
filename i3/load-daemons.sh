aa95c86044f1f803f97000122a5650d2afa3b3e6
Marcelo Freitas <marcelo.freitas@woven-planet.global>
2022-06-16 16:46:10 +0900

    subshell

--- i3/load-daemons.sh
+++ i3/load-daemons.sh
@@ -8,5 +8,5 @@ picom -CGb --experimental-backends &
 nitrogen --restore &
 pactl set-default-sink alsa_output.usb-Yamaha_Corporation_AG06_AG03-00.analog-stereo
 
-sleep 5 && ibus engine mozc-jp 
+( sleep 5 && ibus engine mozc-jp ) &
 
